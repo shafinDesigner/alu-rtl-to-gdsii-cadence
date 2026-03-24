c55d34880dbefd60c3fd257592a65e6d5016f623
shafinDesigner <shafiniutian@icloud.com>
2026-03-25 00:02:13 +0600

    Add files via upload

--- rtl/alu.v
+++ rtl/alu.v
@@ -1,1 +1,25 @@
+`timescale 1ns/1ps
 
+module alu(
+    input  [3:0] A,
+    input  [3:0] B,
+    input  [2:0] opcode,
+    output reg [3:0] result,
+    output reg carry
+);
+
+always @(*) begin
+    case(opcode)
+
+        3'b000: {carry,result} = A + B;
+        3'b001: {carry,result} = A - B;
+        3'b010: result = A & B;
+        3'b011: result = A | B;
+        3'b100: result = A ^ B;
+
+        default: result = 4'b0000;
+
+    endcase
+end
+
+endmodule
